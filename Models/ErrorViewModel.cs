45dff7726e0ef9ddc3f6bdbb695b74a0a601fa4a
DaFray31 <dafray1@gmail.com>
2021-02-03 16:23:04 +0100

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chryscore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
